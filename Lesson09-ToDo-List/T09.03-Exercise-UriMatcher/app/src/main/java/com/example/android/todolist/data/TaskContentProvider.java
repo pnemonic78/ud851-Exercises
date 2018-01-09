0655b482e64c0db450ab9886637d45587ab2aa8c
Moshe <pnemonic@gmail.com>
2018-01-09 11:06:44 +0200

    T09.03 - tests

--- Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.03-Exercise-UriMatcher/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -27,15 +27,15 @@ import android.support.annotation.NonNull;
 // Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
 
-    private static final int TASKS = 100;
-    private static final int TASKS_WITH_ID = 101;
+    public static final int TASKS = 100;
+    public static final int TASK_WITH_ID = 101;
 
     private static final UriMatcher sUriMatcher = buildUriMatcher();
 
-    private static UriMatcher buildUriMatcher() {
+    public static UriMatcher buildUriMatcher() {
         UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
-        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASKS_WITH_ID);
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASK_WITH_ID);
         return uriMatcher;
     }
 
